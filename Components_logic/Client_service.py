1ecde6616c4fcf6559483470187759c2fd0c8ae4
MrCrowley21 <graur.lenka@gmail.com>
2022-10-12 01:23:30 +0300

    Establish communication with Food Ordering System

--- Components_logic/Client_service.py
+++ Components_logic/Client_service.py
@@ -1,11 +1,21 @@
 from time import sleep
 import random
+import requests
+import logging
+from threading import Thread
+
+from config import *
+
+# initialize the logger mode
+logging.basicConfig(level=logging.DEBUG)
 
 
 class ClientService:
     def create_client(self):
         while True:
-            sleep(random.randint(5, 15))
+            sleep(random.randint(5, 15) * time_unit)
+            Thread(target=self.request_menu).start()
 
     def request_menu(self):
-        sleep(1)
+        menu = requests.get(f'{food_ordering_container_url}menu').text
+        logging.info(f'{menu}')
---
+++ config.py
@@ -0,0 +1,6 @@
+# urls
+food_ordering_url = 'http://127.0.0.1:5004/'
+food_ordering_container_url = 'http://food_ordering_container:5004/'
+
+# define constants
+time_unit = 0.5
